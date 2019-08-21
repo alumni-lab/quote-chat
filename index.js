de029164427b0f6b1e5694b958301bdf03685f14
Steven Wing <steven.wing82@gmail.com>
2019-08-21 15:33:45 -0700

    post req

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/quote/', (req, res) => res.send('Hello World!'))
+app.post('/quote', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Quote Chat listening on port ${port}!`))
