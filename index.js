ed332d2fc18ba1118c02ba51f69aa19c43cb3cf7
Steven Wing <steven.wing82@gmail.com>
2019-08-21 15:36:09 -0700

    channel message

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.post('/quote', (req, res) => res.send('Hello World!'))
+app.post('/quote', (req, res) => res.send(
+    {
+        "response_type": "in_channel",
+        "text": "Hello World!",
+        "attachments": [
+            {
+                "text":"Hello to the world!"
+            }
+        ]
+    }
+)
 
 app.listen(port, () => console.log(`Quote Chat listening on port ${port}!`))
