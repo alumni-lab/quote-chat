90a6da35411b6e55250b7155e7544dea287dac88
Steven Wing <steven.wing82@gmail.com>
2019-08-21 14:57:59 -0700

    change res

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.JSON('Hello World!'))
 
 app.listen(port, () => console.log(`Quote Chat listening on port ${port}!`))
