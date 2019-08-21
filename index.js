5ee6469480dac30794d67ea95c8c09be719fffde
Steven Wing <steven.wing82@gmail.com>
2019-08-21 16:54:33 -0700

    query

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const app = express()
 var request = require('request');
+const querystring = require('querystring');
 
 const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.post('/quote', (req, res) => {
-    console.log(req.body)
+    console.log(req.query)
     request.post({
         headers: { 'content-type': 'application/json' }
-        , uri: req.response_url, body: {
+        , url: req.query.response_url, body: {
             "response_type": "in_channel",
             "text": "Hello World!",
             "attachments": [
