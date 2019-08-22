0976111d1adfc15ff4fdaacfb1f254840ebdd320
Steven Wing <steven.wing82@gmail.com>
2019-08-22 11:35:02 -0700

    settimeout added

--- index.js
+++ index.js
@@ -5,26 +5,38 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 const port = process.env.PORT || 5000
 
+
+
+const body = JSON.stringify({
+    "response_type": "in_channel",
+    "text": "Hello World!",
+    "attachments": [
+        {
+            "text": "Hello to the world!"
+        }
+    ]
+});
+
+
+function continueRequest(url) {
+    setTimeout(() => {
+        request.post({
+            headers: { 'content-type': 'application/json' },
+            uri: url,
+            body: body
+        }
+            , function (error, response, body) {
+                console.log(error, response, body);
+            }
+        )
+
+    }, 1000);
+}
+
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/quote', (req, res) => {
     console.log(req.body.response_url)
-    request.post({
-        headers: { 'content-type': 'application/json' }, 
-        uri: req.body.response_url,
-        body: JSON.stringify({
-            "response_type": "in_channel",
-            "text": "Hello World!",
-            "attachments": [
-                {
-                    "text": "Hello to the world!"
-                }
-            ]
-        })
-    }
-        , function (error, response, body) {
-            console.log(error, response, body);
-        }
-    )
+    continueRequest(req.body.response_url);
 
     res.status(200);
 
