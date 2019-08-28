d72a7cd1b1b6711bf36f032499a7c69c9a848d2a
Steven Wing <steven.wing82@gmail.com>
2019-08-28 16:27:40 -0700

    missing res url?

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ app.post('/quote', (req, res) => {
 
 app.post('/api/response', (req, res) => {
     const parsedPayload = JSON.parse(req.body.payload)
-    console.log(parsedPayload.actions[0].value)
+    console.log(parsedPayload.response_url)
     if (parsedPayload.actions[0].value === 'cancel_quote') {
         request.post({
             headers: { 'content-type': 'application/json' },
