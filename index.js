0bdcd1468372f56d716eb35295db7c42c9c38cdc
Steven Wing <steven.wing82@gmail.com>
2019-08-28 14:42:31 -0700

    add res options

--- index.js
+++ index.js
@@ -6,18 +6,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 const port = process.env.PORT || 5000
 
 
-
-const body = JSON.stringify({
-    "response_type": "in_channel",
-    "text": "Hello World!",
-    "attachments": [
-        {
-            "text": "Quote chat slash command awaiting some awesome quotes!!"
-        }
-    ]
-});
-
-
 function continueRequest(url, textToQuote) {
     setTimeout(() => {
         request.post({
@@ -43,13 +31,104 @@ function continueRequest(url, textToQuote) {
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/quote', (req, res) => {
-    if (req.body.text.toLowerCase() == '-help') {   
+    if (req.body.text.toLowerCase() == '-help') {
         res.send({
-            "text" : "Type in your quote and see the magic happen"
+            "text": "Type in your quote and see the magic happen"
         })
     } else {
-        continueRequest(req.body.response_url, req.body.text);
-        res.status(200).send("Requesting quote!")
+        // continueRequest(req.body.response_url, req.body.text);
+
+        res.status(200).send(JSON.stringify([
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "We found *10 Quotes* that match your search*"
+                }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*Your quote option 1*"
+                }
+            },
+            {
+                "type": "context",
+                "elements": [
+                    {
+                        "type": "mrkdwn",
+                        "text": "Quote by _Quote author_ from *Movie name*"
+                    }
+                ]
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*Your quote option 2*"
+                }
+            },
+            {
+                "type": "context",
+                "elements": [
+                    {
+                        "type": "mrkdwn",
+                        "text": "Quote by _Quote author_ from *Movie name*"
+                    }
+                ]
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*Your quote option 3*"
+                }
+            },
+            {
+                "type": "context",
+                "elements": [
+                    {
+                        "type": "mrkdwn",
+                        "text": "Quote by _Quote author_ from *Movie name*"
+                    }
+                ]
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Shuffle options"
+                        },
+                        "value": "load_more_quotes"
+                    },
+                    {
+                        "type": "button",
+                        "style": "danger",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Cancel"
+                        },
+                        "value": "cancel_quote"
+                    }
+                ]
+            }
+        ]))
     }
 
 })
