13ba799c8af34e63be807fe62417fd842ee38ee7
Steven Wing <steven.wing82@gmail.com>
2019-09-18 15:12:25 -0700

    console to find right variable 22

--- index.js
+++ index.js
@@ -362,7 +362,7 @@ app.post('/api/response', async (req, res) => {
       console.log(parsedPayload)
       const yourQuote = await getDetails(parsedPayload.actions[0].value.slice(12))
       console.log("#############################")
-      console.log(yourQuote.quote)
+      console.log(yourQuote)
       res.sendStatus(200)
       let choice = yourQuote.quote
       continueRequest(parsedPayload.response_url, parsedPayload.channel.id, choice)
