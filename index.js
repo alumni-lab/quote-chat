a783968256abc0b64b1d7a38ce3f9da48bbb2fb0
Steven Wing <steven.wing82@gmail.com>
2019-09-18 17:07:54 -0700

    set username

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ function continueRequest(clearUrl, reply_to, quoteText, quoteChar, quoteMovie) {
     body: JSON.stringify({
       "channel": reply_to,
       "as_user": false,
-      "username": 
+      "username": 'quote-chat',
       "delete_original": "true",
       "blocks": [{
         "type": "section",
