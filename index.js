895dc0d18978732e79db3064a493724416073970
Ben Deltenre <benjdelt@gmail.com>
2019-10-03 18:51:57 -0700

    Updated stopWords array

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ async function dbQuery(quote) {
       "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there",
       "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no",
       "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don",
-      "should", "now"];
+      "should", "now", ""];
     const quoteSplit = quote.split(' ').filter(function(word) {
       return !stopWords.includes(word);
     })
