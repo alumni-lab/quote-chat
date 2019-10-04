8055d4b942ecbc56c481b31df61dabc912072026
Steven Wing <steven.wing82@gmail.com>
2019-10-03 19:02:01 -0700

    get auth details 12

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ const clientSecret = process.env.CLIENT_SECRET
 app.get('/auth', async (req, res) => {
   // console.log('origin', req.query)
   let stupidThing = await getBotId(req.query.code)
-  setTimeout(() => {
-    console.log('WTF:', stupidThing)
-  }, 3000);
+  console.log('WTF:', stupidThing)
   res.status(200)
 })
 
@@ -40,6 +38,7 @@ async function getBotId(code) {
       access_token: body.access_token,
       bot_access_token: body.bot_access_token
     }
+    console.log(authAccess, '/nXXX')
   })
   return authAccess
 }
@@ -88,13 +87,13 @@ async function dbQuery(quote) {
       "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they",
       "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am",
       "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing",
-      "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", 
+      "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with",
       "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from",
       "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there",
       "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no",
       "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don",
       "should", "now", ""];
-    const quoteSplit = quote.split(' ').filter(function(word) {
+    const quoteSplit = quote.split(' ').filter(function (word) {
       return !stopWords.includes(word);
     })
     let i = 0
