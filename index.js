a4e42f3202487338fcae41978b6a6fe98e6610b6
Ben Deltenre <benjdelt@gmail.com>
2019-10-03 18:46:59 -0700

    Filtered out stop words from query

--- index.js
+++ index.js
@@ -75,10 +75,22 @@ async function dbQuery(quote) {
     const result = await client.query(`SELECT * FROM characters WHERE id = ${row.character_id};`)
     quo.character = result.rows[0].name
     exactQuote = quo;
-    // quoteList.push(quo)
   }
   if (quoteList.length < 4) {
-    const quoteSplit = quote.split(' ')
+    const stopWords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+      "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they",
+      "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am",
+      "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing",
+      "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", 
+      "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from",
+      "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there",
+      "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no",
+      "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don",
+      "should", "now"];
+    const quoteSplit = quote.split(' ').filter(function(word) {
+      return stopWords.includes(word);
+    })
+    console.log("quoteSplit:", quoteSplit);
     let i = 0
     while (quoteList.length < 30) {
       if (i < quoteSplit.length) {
