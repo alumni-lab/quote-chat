16dd6e39b9c43643ccf15b859c2d67ac24bffa97
Steven Wing <steven.wing82@gmail.com>
2019-10-03 20:08:57 -0700

    db connect 9

--- index.js
+++ index.js
@@ -150,8 +150,8 @@ async function getChar(id) {
 
 async function continueRequest(clearUrl, reply_to, quoteText, quoteChar, quoteMovie, userName, teamID) {
   // clear origin message
-  const teamAuth = await client.query(`SELECT * FROM auth WHERE team_id = '${teamID}'`)
-  console.log(teamAuth)
+  let teamAuth = await client.query(`SELECT * FROM auth WHERE team_id = '${teamID}'`)
+  teamAuth = teamAuth.rows[0]
   qcToken = teamAuth.access_token
   bot_access_token = teamAuth.bot_access_token
   console.log('qc = ', qcToken, '\nbot = ', bot_access_token)
